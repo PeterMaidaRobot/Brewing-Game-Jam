4499ce163a090ba636fb9467217a76b3ffaa3459
Peter Maida <pmaida1357@gmail.com>
2026-04-12 01:03:57 -0400

    made my own smoke sprite

--- cauldron.gd
+++ cauldron.gd
@@ -4,13 +4,51 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	create_initial_smoke_cloud()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:	
 	pass
 
+
+func create_initial_smoke_cloud() -> void:
+	
+	for i in range(100):
+	
+		# Create a new instance of the Mob Scene
+		var smoke = smoke_scene.instantiate()
+		
+		# Continue the circle along the Path2D at an interval, it will reset to zero once it goes over 1
+		var smoke_spawn_location = $SmokePath/SmokeSpawnLocation
+		smoke_spawn_location.progress_ratio = smoke_spawn_location.progress_ratio + 0.01
+		
+		# Set the mob's position to the location
+		smoke.position = smoke_spawn_location.position
+		
+		# Set the mob's direction prependicular to the path direction.
+		var direction = smoke_spawn_location.rotation #- (PI / 2)
+		#smoke_spawn_location.rotation += 0.06
+		
+		var layer = randf_range(1.0, 100.0)
+		smoke.position = smoke_spawn_location.position + Vector2(layer, layer).rotated(direction)
+		
+		# Add some randomness to the direction.
+		#direction += randf_range(-PI / 4, PI / 4)
+		smoke.rotation = direction
+		
+		# Choose the velocity for the mob.
+		var velocity = Vector2(randf_range(10.0, 20.0), 0.0)
+		smoke.linear_velocity = velocity.rotated(direction)
+		
+		# Spawn the mob by adding it to the Main scene.
+		add_child(smoke)
+		#print(smoke_spawn_location.progress_ratio)
+		#print(smoke.position)
+		#print(smoke.rotation)
+	
+	
+	
 
 func _on_smoke_timer_timeout() -> void:
 	# Create a new instance of the Mob Scene
