f6cf3dc69d65169307a3408e9798bca8b9ea2e01
Peter Maida <pmaida1357@gmail.com>
2026-04-18 19:39:00 -0400

    connected cutscene to godot

--- scripts/levels/cutscene.gd
+++ scripts/levels/cutscene.gd
@@ -19,8 +19,33 @@ func _process(delta: float) -> void:
 
 func update_panel(new_panel_index : int) -> void:
 	
-	match new_panel_index: # TODO there's def a better way than this...
+	match new_panel_index:
+		# TODO there's def a better way than this... though I don't want to do get_node("Cutscene" + index) for lack of syntax checking
 		2:
-			$Panels/BigFacePanel.show()
+			$Panels/Cutscene2.show()
 		3:
+			$Panels/Cutscene3.show()
+		4:
+			$Panels/Cutscene4.show()
+		5:
+			$Panels/Cutscene5.show()
+		6:
+			$Panels/Cutscene6.show()
+		7:
+			$Panels/Cutscene7.show()
+		8:
+			$Panels/Cutscene8.show()
+		9:
+			$Panels/Cutscene9.show()
+		10:
+			$Panels/Cutscene10.show()
+		11:
+			$Panels/Cutscene11.show()
+		12:
+			$Panels/Cutscene12.show()
+		13:
+			$Panels/Cutscene13.show()
+		14:
+			$Panels/BigFacePanel.show()
+		15:
 			get_tree().change_scene_to_file(Constants.LEVEL_SCENE_PATH[0])
