73be1c1b8aed9b3d306ca97b0a167c82597cb566
Peter Maida <pmaida1357@gmail.com>
2026-04-20 22:37:36 -0400

    Changed main menu font, improved read me, and added a back button to the level select

--- scripts/levels/main_menu.gd
+++ scripts/levels/main_menu.gd
@@ -16,10 +16,15 @@ func _process(delta: float) -> void:
 func _on_new_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/cutscene.tscn")
 
-
+# Navigate from the Main Menu to the Level Select screen
 func _on_level_select_button_pressed() -> void:
 	$LevelScreen.show()
 	$MenuScreen.hide()
+
+# Navigate back from the Level Select screen to the main menu
+func _on_back_button_pressed() -> void:
+	$MenuScreen.show()
+	$LevelScreen.hide()
 
 func _on_level_button_pressed(level_num: int) -> void:
 	get_tree().change_scene_to_file(Constants.LEVEL_SCENE_PATH[level_num])
