4333b9e4b7dffd745b9f82a433453e0fbb6a4356
Peter Maida <pmaida1357@gmail.com>
2026-04-25 14:55:07 -0400

    made level 6!

--- scenes/butterfly.gd
+++ scenes/butterfly.gd
@@ -7,7 +7,8 @@ class_name Butterfly
 func _ready() -> void:
 	$AnimatedSprite2D.animation = "fly"
 	$AnimatedSprite2D.play()
-
+	# Pick a random frame
+	$AnimatedSprite2D.frame = randi_range(0, $AnimatedSprite2D.sprite_frames.get_frame_count("fly"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,9 +16,11 @@ func _process(delta: float) -> void:
 	# Call the process for Airbornes:
 	super._process(delta)
 	
-	if linear_velocity.length() < 10:
-		linear_velocity += Vector2(randf_range(-0.5, 0.5),
+	# Get a random direction, if it goes over the max speed, don't add it
+	var new_dir : Vector2 = Vector2(randf_range(-0.5, 0.5),
 								   randf_range(-0.5, 0.5))
+	if (linear_velocity+new_dir).length() < 6:
+		linear_velocity += new_dir
 
 
 
--- scripts/constants.gd
+++ scripts/constants.gd
@@ -7,5 +7,7 @@ const LEVEL_SCENE_PATH : Dictionary[int, String] = {
 	2: "res://scenes/levels/level_2.tscn",
 	3: "res://scenes/levels/level_3.tscn",
 	4: "res://scenes/levels/level_4.tscn",
+	5: "res://scenes/levels/level_5.tscn",
+	6: "res://scenes/levels/level_6.tscn",
 	99: "res://scenes/levels/finale.tscn"
 }
